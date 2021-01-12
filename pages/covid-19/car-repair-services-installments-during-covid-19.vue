f135d5cd5d59a86ba70bce3d29d79e4c5900d520
ammar <ammar@manyautos.co.uk>
2021-01-11 18:04:30 -0800

    changes

--- pages/covid-19/car-repair-services-installments-during-covid-19.vue
+++ pages/covid-19/car-repair-services-installments-during-covid-19.vue
@@ -3,19 +3,21 @@
     title='Car Repair During COVID 19 | Car Repair Instalment Plan'
     heading-bg='customer-v2/static-pages/registergarages.jpg'
   >
-    <v-container>
+    <v-container class="covid-page" style="padding-top:50px;">
       <v-row>
         <div class="col-12">
-          <div class="row align-items-center pb-4 text-md-right text-center booking">
-            <div class="col-md-6 margin-top-25 order-md-2 text-left">
-              <img src="https://static.manyautosltd.com/uploads/car-repair-services-installments-during-covid-19.jpg" alt="car repair services installments during covid 19">
+          <div class="row align-items-center pb-4  text-center booking">
+            <div class="col-md-6 margin-top-25 order-md-2">
+              <img src="https://static.manyautosltd.com/uploads/pexels-matheus-bertelli-799443.jpg" alt="car repair services installments during covid 19" class="image-one">
 
             </div>
-            <div class="col-md-6 margin-top-5-per" style="margin-top:-35px;">
-              <h2 class="green2">You Do The Booking</h2>
-              <p class="text-teal text-22">It couldn't be easier to get started. Simply pick up the phone or book
-                online. Enter your vehicle registration, postcode and follow the simple steps to tell us what service
-                you require.</p>
+            <div class="col-md-6 margin-top-5-per sec-1">
+              <h2 class="green2">Helping You To Get Back On The Road</h2>
+              <p class="text-teal text-22 mt-4">We always say that customers are our topmost priority. In critical times like
+                this, we want to help you in any way we can.</p>
+              <p class="text-teal text-22">With the support from our network of garages, we are following all the social
+                distancing measures and offering Auto repair payment plans so you can have a safe and affordable
+                experience.</p>
 
             </div>
           </div>
@@ -23,17 +25,13 @@
 
         <div class="col-12">
           <div class="row align-items-center pb-4 text-center booking">
-            <div class="col-md-6 text-md-right">
-              <img :src="assets('customer-v2/static-pages/collection.jpg')" alt="We Do The Collection">
+            <div class="col-md-6 ">
+              <img src="https://static.manyautosltd.com/uploads/open-in-covid19.jpg" alt="open-in-covid19" class="image-two">
 
             </div>
-            <div class="col-md-6 margin-top-5-per" style="margin-top:-35px;">
-              <h2 class="green2">We Do The Collection</h2>
-              <p class="text-teal text-22">
-                Take advantage of our free collection & delivery service from
-                home or work. If your vehicle is not road worthy, we can arrange
-                a recovery service for you.
-              </p>
+            <div class="col-md-6 margin-top-5-per sec-2">
+              <h2 class="green2">We Are Open And Unaffected by COVID-19 </h2>
+              <p class="text-teal text-22 mt-4">Our garages are open and providing the services to maintain your car during this pandemic. We are taking all the social distancing measures to protect our customers as well as our employees. You can book any service online as we are always available digitally.</p>
             </div>
           </div>
         </div>
@@ -118,5 +116,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.covid-page .green2{
+  font-weight: 300;
+}
+@media (min-width: 960px){
+  .covid-page .booking .sec-2 {
+  text-align: left;
+}
+ .covid-page .booking .sec-1 {
+  text-align: right;
+}
+}
+@media (min-width: 560px) and (max-width:960px){
+  .covid-page .booking .image-one{
 
+    max-width: 57%;
+    text-align: center;
+  }
+  .covid-page .booking .image-two{
+
+    max-width: 60%;
+  }
+}
+@media (min-width: 1260px){
+  .covid-page .booking .image-one{
+
+    max-width: 70%;
+    text-align: center;
+  }
+  .covid-page .booking .image-two{
+
+    max-width: 70%;
+  }
+}
 </style>
