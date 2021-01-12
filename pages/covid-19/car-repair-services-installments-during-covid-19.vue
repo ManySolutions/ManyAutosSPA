a8cf7510cc64e66a380feab494334ab5c8cb42f7
ammar <ammar@manyautos.co.uk>
2021-01-12 11:36:02 -0800

    changes

--- pages/covid-19/car-repair-services-installments-during-covid-19.vue
+++ pages/covid-19/car-repair-services-installments-during-covid-19.vue
@@ -37,6 +37,96 @@
         </div>
       </v-row>
     </v-container>
+      <v-container 
+    class='covid-sec-2'
+  >
+    <v-row>
+      <v-col cols=12 class="text-center mb-7 pb-lg-5">
+        <p style='max-width:700px' class='m-auto mb-5 mb-md-10'>
+          We will ask you to help us a little by following the necessary measures with us
+        </p>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols=12 lg=3 xl='4' order-lg=2 class="text-center">
+        <!-- <img 
+          :src="assets('customer-v2/011.png')" 
+          alt="Our Services at ManyAutos LTD"
+          class='pb-5 pt-4'
+        > -->
+          
+      </v-col>
+
+      <v-col cols=12 lg=3 order-lg=1>
+        <div class="media text-left">
+          <div class="media-body">
+            <h6 class="position-relative d-lg-flex align-items-lg-end justify-content-lg-end text-center">
+              <i class="text-primary ico-ma-check-circle ml-lg-2 mr-2 mr-lg-0 order-lg-2"></i>
+              <span class="order-lg-1"> Social distancing</span>
+            </h6>
+            <p class='text-center ml-lg-auto mr-lg-4 pr-lg-2 text-lg-right m-auto'>
+              Social distancing rules are followed in our garage. Every person has to keep a 2-meter distance from each
+              other and avoid any kind of contact.
+            </p>
+          </div>
+        </div>
+
+        <div class="media text-right">
+          <div class="media-body">
+            <h6 class="position-relative d-lg-flex align-items-lg-end justify-content-lg-end text-center">
+              <i class="text-primary ico-ma-check-circle ml-lg-2 mr-2 mr-lg-0 order-lg-2"></i>
+              <span class=""> Masks are compulsory</span>
+            </h6>
+            <p class='m-auto text-center ml-lg-auto mr-lg-4 pr-lg-2 text-lg-right'>
+              All the employees and customers are supposed to wear masks at all times. You won’t be allowed to enter the
+              garage without a face mask.
+            </p>
+          </div>
+        </div>
+      </v-col>
+
+      <v-col cols=12 lg=3 order-lg=3 >
+        <div class="media-second text-left">
+          <div class="media-body">
+            <h6 class="position-relative d-lg-flex align-items-lg-end text-center">
+              <i class="text-primary ico-ma-check-circle mr-2"></i>
+              <span class="">Hygiene is a priority</span>
+            </h6>
+            <p class='text-center m-auto text-lg-left ml-lg-4 pl-lg-2'>
+              All of our employees are extra careful about hygiene. They wash their hands and sanitize them at all
+              times. We sanitize your car before and after it has been worked on in our garage.
+            </p>
+          </div>
+        </div>
+        
+        <div class="media text-right">
+          <div class="media-body mb-0">
+            <h6 class="position-relative d-lg-flex align-items-lg-end text-center">
+              <i class="text-primary ico-ma-check-circle mr-2"></i>
+              <span class="">Protective gear</span>
+            </h6>
+            <p class='text-center m-auto text-lg-left ml-lg-4 pl-lg-2'>
+              We offer a no-contact service; our employees will be provided fresh gloves for each car servicing. 
+            </p>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+     <v-row>
+      <v-col cols=12 class="text-center mb-7 pb-lg-5 media-body-last">
+        <div class="media-body" style="max-width:700px; margin:auto;">
+            <h6 class=" text-center mt-12">
+              <i class="text-primary ico-ma-check-circle mr-2"></i>
+              <span class="">Checking for everyone’s safety</span>
+            </h6>
+            <p class='text-center'>
+              We will check our employee's fever every day and look out for any symptoms regarding covid-19. This will
+              be done for your safety and your peace of mind. 
+            </p>
+          </div>
+      </v-col>
+    </v-row>
+  </v-container>
   </page-layout>
 </template>
 
@@ -148,5 +238,77 @@ export default {
 
     max-width: 70%;
   }
+}
+.covid-sec-2 .media-body h6{
+    color: #06182d;
+    font-size: 17px;
+    font-weight: 700;
+    letter-spacing: 0;
+    margin-bottom: 0.9em;
+    font-family: inherit;
+    text-transform: uppercase;
+}
+.covid-sec-2 .media .media-body h6{
+    color: #06182d;
+    font-size: 17px;
+    font-weight: 700;
+    letter-spacing: 0;
+    margin-bottom: 0.9em;
+    font-family: inherit;
+    text-transform: uppercase;
+}
+.covid-sec-2 .media-body h6 i{
+    font-size: 40px;
+    vertical-align: sub;
+}
+.covid-sec-2 .media-second .media-body h6{
+    color: #06182d;
+    font-size: 17px;
+    font-weight: 700;
+    letter-spacing: 0;
+    margin-bottom: 0.9em;
+    font-family: inherit;
+    text-transform: uppercase;
+}
+.covid-sec-2 .media-second .media-body h6 i{
+    font-size: 40px;
+    vertical-align: sub;
+}
+@media (max-width:1263px){
+  .covid-sec-2 .media-second{
+    margin-top: -20px;
+}
+}
+.covid-sec-2 .media .media-body h6 i{
+    font-size: 40px;
+    vertical-align: sub;
+}
+@media (max-width:1263px){
+  .covid-sec-2 .media .media-body{
+    margin-bottom: 50px;
+}
+}
+@media (max-width:1263px){
+  .covid-sec-2 .media-second .media-body{
+    margin-bottom: 50px;
+}
+}
+.covid-sec-2 .media-body-last .media-body h6{
+    color: #06182d;
+    font-size: 17px;
+    font-weight: 700;
+    letter-spacing: 0;
+    margin-bottom: 0.9em;
+    font-family: inherit;
+    text-transform: uppercase;
+}
+.covid-sec-2  .media-body-last .media-body h6 i{
+    font-size: 40px;
+    vertical-align: sub;
+}
+@media (max-width:1263px){
+  .covid-sec-2 .media-body-last {
+    margin-top: -24px;
+}
 }
 </style>
