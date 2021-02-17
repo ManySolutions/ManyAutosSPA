cc1d77a1bec8b46113d71a69de94725d9798f1cf
aqeel <smaqeelkazmi@gmail.com>
2021-02-17 17:47:07 +0500

    fix

--- layouts/content-admin-layout.vue
+++ layouts/content-admin-layout.vue
@@ -14,7 +14,7 @@
             Content Admin Panel
           </v-list-item-title>
           <v-list-item-subtitle>
-            {{info.name}}
+            {{info.name || ''}}
           </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
