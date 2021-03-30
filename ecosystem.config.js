c4285996840b1162f8e121a1e77f7f40102a7b47
aqeel <smaqeelkazmi@gmail.com>
2021-03-30 16:44:47 +0500

    added changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: 'NuxtAppName',
+      name: 'ManyAutosReading',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start'
+      args: 'start',
+      port: 0610
     }
   ]
 }
--- staging.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-npm install #install packages
-# rmdir "./node_modules/.cache/hard-source" /s /q 
-rm -rf "./node_modules/.cache/hard-source"
-npm run build:staging # generate build
-pm2 start ecosystem.staging.config.js # start app
