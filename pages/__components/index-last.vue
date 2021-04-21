b4e51a39bc21130282d7cb9be93cf29b222697a8
ammar <ammar@manyautos.co.uk>
2021-04-21 12:27:10 -0800

    changes

--- pages/__components/index-last.vue
+++ pages/__components/index-last.vue
@@ -8,21 +8,19 @@
           </div>
           <div class="para-one">
             <p>
-              We offer a full range of garage services to vehicle owners located
-              in Tucson area. All mechanic services are performed by highly
-              qualified mechanics. We can handle any car problem.
+              We are a one-stop shopping for all your automotive needs. We provide a huge range of car services and
+              repairs. All the services are carried out by highly trained mechanics. We can handle any automotive
+              problem.
             </p>
           </div>
           <div class="para-two">
             <p>
-              We offer a full range of garage services to vehicle owners in
-              Tucson. Our professionals know how to handle a wide range of car
-              services. Whether you drive a passenger car or medium sized truck
-              or SUV, our mechanics strive to ensure that your vehicle will be
-              performing at its best before leaving our car shop.
+              We have always considered customers our highest priority and we want to provide them with the best
+              services. It doesn’t matter what type of car do you own, whether it’s a SUV, a passenger’s car or a truck.
+              Our professionals will make sure that your car is in a top-notch condition before you leave our garage.
             </p>
           </div>
-          <div class="text-left">
+          <div class="text-left mb-12 mb-md-0">
             <v-btn 
             color="primary" 
             x-large
