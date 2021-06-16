917319fe241297b45712cb1522dbb4d030c2cc6d
aqeel <smaqeelkazmi@gmail.com>
2021-06-16 11:26:28 +0500

    c

--- nuxt.config.staging.js
+++ nuxt.config.staging.js
@@ -217,7 +217,7 @@ export default {
     CURRENCY_SYMBOL: '£',
     CURRENCY_CODE: 'GBP',
     APP_URL: 'https://staging-2021.manyautosltd.com',
-    API_URL: 'http://manyautos.local/api/v2/c/',
+    API_URL: 'https://staging-v32020.manyautos.co.uk/api/v2/c/',
     ASSETS_URL: 'https://app.manyautosltd.com/assets/',
     RECAPTCHA_SITE_KEY: '6LdIdBcaAAAAAGoHmtHlYIa7vSFbJ4Ja0_gQCBlX',
     FBQ_TEST_CODE: 'TEST27350',
